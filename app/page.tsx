51f6fd1e3e77d07a6375d1d688cdbbbfeb1176ea
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2023-03-15 13:04:58 +0800

    fix: update loading state when error occurred

--- app/page.tsx
+++ app/page.tsx
@@ -81,6 +81,7 @@ export default function Home() {
             setCompletedQuiz(completedQuiz);
         } catch (error) {
             console.log(error);
+            channel?.broadcast('loadingState', { isLoading: false });
             setIsShowLoading(false);
         }
 
