8497df8a9bf77fb9763f4228158b2bddfd53a8eb
Viskeyy <50070461+Viskeyy@users.noreply.github.com>
2023-03-18 00:15:36 +0800

    style: modify some styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,35 @@
 'use client';
-import {useState, useEffect} from 'react';
+import { useState, useEffect, useRef, ChangeEvent } from 'react';
 import Image from 'next/image';
-import {createPresence} from '@yomo/presence';
-import {Loading} from '@/components/Loading';
+import { createPresence } from '@yomo/presence';
+import { Loading } from '@/components/Loading';
 
 export default function Home() {
     const [channel, setChannel] = useState<any>(null);
+
+    // messages used to sending to presence , displayMessages used to show the messages form presence
     const [messages, setMessages] = useState<Message[]>([]);
     const [displayMessages, setDisplayMessages] = useState<Message[]>([]);
+
+    // user input used to sending openai request , input state used to show the messages from presencs and contorl the input state , loadingState used to display the openai request state
     const [userInput, setUserInput] = useState<string>('');
+    // const [inputState, setInputState] = useState<InputState>();
     const [loadingState, setLoadingState] = useState<boolean>(false);
 
+    // used to auto scroll to bottom
+    const bottomDiv = useRef<HTMLDivElement>(null);
+    const scrollToBottom = () => {
+        bottomDiv.current?.scrollIntoView({ behavior: 'smooth' });
+    };
+
     const currentConnectId = (
-        Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000
+        Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000
     ).toString();
+
+    // const currentUser: UserInfo = {
+    //     name: 'user' + currentConnectId,
+    //     color: `#${currentConnectId}`,
+    // };
 
     const presenceConnect = async () => {
         const presence = await createPresence({
@@ -41,6 +57,15 @@ export default function Home() {
             }
         );
 
+        // joinChannel?.subscribe(
+        //     'inputState',
+        //     (message: { inputState: InputState }) => {
+        //         console.log(message.inputState, 'receive input state');
+        //         setInputState(message.inputState);
+        //         setUserInput(message.inputState.content);
+        //     }
+        // );
+
         setChannel(joinChannel);
     };
 
@@ -49,7 +74,7 @@ export default function Home() {
 
         const updateMessages = [
             ...messages,
-            {role: 'user' as const, content: userInput},
+            { role: 'user' as const, content: userInput },
         ];
 
         setMessages(updateMessages);
@@ -57,27 +82,33 @@ export default function Home() {
         channel?.broadcast('chatInfo', {
             messages: updateMessages,
         });
-        channel?.broadcast('loadingState', {isLoading: true});
+        channel?.broadcast('loadingState', { isLoading: true });
 
         const response = await fetch('/api/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({message: userInput}),
+            body: JSON.stringify({ message: userInput }),
         });
 
+        // channel?.broadcast('inputState', {
+        //     inputState: {},
+        // });
+
         if (!response.ok) {
-            channel?.broadcast('loadingState', {isLoading: false});
+            channel?.broadcast('loadingState', { isLoading: false });
             return;
         }
 
         const data = response.body;
 
         if (!data) {
-            channel?.broadcast('loadingState', {isLoading: false});
+            channel?.broadcast('loadingState', { isLoading: false });
             return;
         }
+
+        setUserInput('');
 
         const reader = data.getReader();
         const decoder = new TextDecoder();
@@ -86,7 +117,7 @@ export default function Home() {
         let isFirst = true;
 
         while (!done) {
-            const {value, done: doneReading} = await reader.read();
+            const { value, done: doneReading } = await reader.read();
             done = doneReading;
             const chunkValue = decoder.decode(value);
 
@@ -94,12 +125,12 @@ export default function Home() {
                 isFirst = false;
                 setMessages((messages) => [
                     ...messages,
-                    {role: 'assistant', content: chunkValue},
+                    { role: 'assistant', content: chunkValue },
                 ]);
                 channel?.broadcast('chatInfo', {
                     messages: [
                         ...messages,
-                        {role: 'assistant', content: chunkValue},
+                        { role: 'assistant', content: chunkValue },
                     ],
                 });
             } else {
@@ -119,8 +150,22 @@ export default function Home() {
             }
         }
 
-        channel?.broadcast('loadingState', {isLoading: false});
+        channel?.broadcast('loadingState', { isLoading: false });
     };
+
+    // const syncInputState = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    //     console.log(
+    //         { ...currentUser, content: event.target.value },
+    //         'send input state'
+    //     );
+    //     console.log(inputState, 'input state brfore send');
+    //     channel?.broadcast('inputState', {
+    //         inputState: {
+    //             ...currentUser,
+    //             content: event.target.value,
+    //         },
+    //     });
+    // };
 
     useEffect(() => {
         if (window) {
@@ -131,14 +176,18 @@ export default function Home() {
         };
     }, []);
 
+    useEffect(() => {
+        scrollToBottom();
+    }, [displayMessages]);
+
     return (
         <main>
             <div className='flex flex-col h-screen overflow-auto items-center w-2/3 mx-auto'>
-                <div className='text-gray-400 text-5xl sticky top-0 my-8'>
+                <div className='text-gray-400 text-3xl sticky top-0 my-8'>
                     Presence real-time showcase
                 </div>
 
-                <div className='w-1/2 p-4 h-[80vh] overflow-y-auto'>
+                <div className='w-full p-4 h-[80vh] overflow-y-auto'>
                     <div className='flex flex-col rounded-lg border-neutral-300'>
                         {displayMessages.map((message, index) => {
                             return (
@@ -156,7 +205,7 @@ export default function Home() {
                                                 ? 'bg-neutral-200 text-neutral-900'
                                                 : 'bg-blue-500 text-white'
                                         } rounded-2xl px-4 py-2 max-w-[67%] whitespace-pre-wrap`}
-                                        style={{overflowWrap: 'anywhere'}}
+                                        style={{ overflowWrap: 'anywhere' }}
                                     >
                                         {message.content}
                                     </div>
@@ -174,31 +223,59 @@ export default function Home() {
                                         overflowWrap: 'anywhere',
                                     }}
                                 >
-                                    <Loading isShow={loadingState}/>
+                                    <Loading isShow={loadingState} />
                                 </div>
                             </div>
                         </div>
                     )}
+
+                    <div ref={bottomDiv}></div>
                 </div>
 
-                <div className='flex flex-nowrap items-center justify-center relative w-1/2 my-8'>
+                <div className='flex flex-nowrap items-center justify-center relative w-full my-8'>
                     <textarea
                         className='min-h-12 rounded-lg p-2 w-full focus:outline-none focus:ring-1 focus:ring-neutral-300 border-2 border-neutral-200'
-                        style={{resize: 'none'}}
+                        style={{ resize: 'none' }}
                         placeholder='Type a message...'
+                        // value={inputState?.content}
                         value={userInput}
                         rows={1}
-                        onChange={(e) => {
-                        setUserInput(e.target.value);
-                    }}
+                        onChange={(e) => setUserInput(e.target.value)}
+                        // onChange={(event) => syncInputState(event)}
+                        /** onKeyDown={(event: KeyboardEvent<HTMLTextAreaElement>) => {
+                            if (event.key === 'Enter' && event.ctrlKey) {
+                                submitInput();
+                            }
+                        }} **/
                     />
-                    <button onClick={submitInput} disabled={loadingState} className='absolute right-2'>
+                    <button
+                        onClick={submitInput}
+                        disabled={loadingState || true}
+                        // disabled={loadingState || !!inputState?.name}
+                        className='absolute right-2'
+                        // style={{ color: inputState?.color }}
+                    >
+                        {/* {inputState?.name ? (
+                            `${inputState.name} is typing...`
+                        ) : (
+                            <Image
+                                src={'/arrow-up.svg'}
+                                alt='send arrow'
+                                width={32}
+                                height={32}
+                                className='hover:cursor-pointer rounded-full p-1 bg-blue-500 text-white hover:opacity-80'
+                            />
+                        )} */}
                         <Image
                             src={'/arrow-up.svg'}
                             alt='send arrow'
                             width={32}
                             height={32}
-                            className='hover:cursor-pointer rounded-full p-1 bg-blue-500 text-white hover:opacity-80'
+                            className={`${
+                                loadingState
+                                    ? 'hover:cursor-not-allowed'
+                                    : 'hover:cursor-pointer'
+                            } rounded-full p-1 bg-blue-500 hover:opacity-80`}
                         />
                     </button>
                 </div>
--- global.d.ts
+++ global.d.ts
@@ -4,3 +4,14 @@ interface Message {
     role: Role;
     content: string;
 }
+
+interface UserInfo {
+    name: string,
+    color: string,
+}
+
+interface InputState extends UserInfo {
+    content: string,
+}
+
+
