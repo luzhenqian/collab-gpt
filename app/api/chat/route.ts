f232898d4870c53f32c17f126e2a9056500c6111
Noah Lu <15753140326@163.com>
2023-03-20 13:27:51 +0800

    test

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,18 +3,15 @@ import { openAIStream } from "@/helper/openAIStream";
 export const config = {
   runtime: "edge",
 };
-export default async (req: Request) => {
-  console.log(req, " req");
-  if (req.method !== "POST") {
-    return new Response("Method not allowed", { status: 405 });
-  }
-
+export async function POST(req: Request) {
   try {
     const { message } = await req.json();
     const stream = await openAIStream(message);
     return new Response(stream);
   } catch (e) {
+    console.log(req, " req");
+
     console.log(e);
     return new Response("Error", { status: 500 });
   }
-};
+}
